d87441a4babe2ced09dcd869415a46cadafd546e
Matthew Greco <mgreco2112@tutanota.com>
2023-05-18 19:11:10 -0400

    added a restart game button that reloads the scene. Implemented the SceneManager namespace to utilize the LoadScene method, as the tutorial has since been depricated

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
     public const int gridRows = 2;
@@ -43,6 +44,10 @@ public class SceneController : MonoBehaviour {
 
         _firstRevealed = null;
         _secondRevealed = null;
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene("Scene");
     }
 
 
